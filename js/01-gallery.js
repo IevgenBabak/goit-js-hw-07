16e7ebb5bbd1da1a78d89ef4232666ca797b0a7f
Ievgen Babak <ievgenbabak@gmail.com>
2023-01-08 11:25:30 +0100

    last

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,23 +6,24 @@ const gallery = document.querySelector('.gallery')
 const items = []
 
 galleryItems.forEach(element => {
-	const galleryItem = document.createElement('div')
-	galleryItem.className = 'gallery__item'
-	const galleryLink = document.createElement('a')
-	galleryLink.className = 'gallery__link'
-	galleryLink.href = element.original
-	const galleryImage = document.createElement('img')
-    galleryImage.className = 'gallery__image'
+	const galleryItem = document.createElement('div');
+	galleryItem.className = 'gallery__item';
+	const galleryLink = document.createElement('a');
+	galleryLink.className = 'gallery__link';
+	galleryLink.href = element.original;
+	const galleryImage = document.createElement('img');
+    galleryImage.className = 'gallery__image';
     galleryImage.src = element.preview;
-    galleryImage.setAttribute('data-source', element.original)
+    galleryImage.setAttribute('data-source', element.original);
     galleryImage.alt = element.description;
 
-	galleryItem.append(galleryLink)
-	galleryLink.append(galleryImage)
-	items.push(galleryItem)
+	galleryItem.append(galleryLink);
+	galleryLink.append(galleryImage);
+	items.push(galleryItem);
 })
 
-gallery.append(...items)
+
+gallery.append(...items);
 
 gallery.addEventListener('click', e => {
     e.preventDefault();
